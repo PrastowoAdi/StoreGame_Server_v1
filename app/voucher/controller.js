10bbaf750b2d4ce8fe3173d8f8776df3d662d2e5
PrastowoAdi <sinaadi75@gmail.com>
2022-03-06 15:00:38 +0700

    Add feature delete data and edit status voucher in page Voucher

--- app/voucher/controller.js
+++ app/voucher/controller.js
@@ -185,22 +185,48 @@ module.exports = {
         }
     },
 
-    // actionDelete: async(req, res) => {
-    //     try {
-    //         const { id } = req.params;
+    actionDelete: async(req, res) => {
+        try {
+            const { id } = req.params;
 
-    //         await Nominal.findOneAndRemove({
-    //             _id: id
-    //         });
+            const voucher = await Voucher.findOneAndRemove({
+                _id: id
+            });
 
-    //         req.flash("alertMessage", "Success Delete Nominal");
-    //         req.flash("alertStatus", "success");
+            let currentImage = `${config.rootPath}/public/uploads/${voucher.thumbnail}`;
+            if(fs.existsSync(currentImage)){
+                fs.unlinkSync(currentImage)
+            }
+            req.flash("alertMessage", "Success Delete Voucher");
+            req.flash("alertStatus", "success");
 
-    //         res.redirect('/nominal');
-    //     } catch (err) {
-    //         req.flash('alertMessage', `${err.message}`);
-    //         req.flash('alertStatus', 'danger');
-    //         res.redirect('/nominal');
-    //     }
-    // }
+            res.redirect('/voucher');
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`);
+            req.flash('alertStatus', 'danger');
+            res.redirect('/voucher');
+        }
+    },
+
+    actionStatus: async(req, res) => {
+        try {
+            const { id } = req.params;
+            let voucher = await Voucher.findOne({_id: id});
+
+            let status = voucher.status === 'Y' ? 'N' : 'Y';
+
+            voucher = await Voucher.findOneAndUpdate({
+                _id: id
+            }, {status});
+
+            req.flash("alertMessage", "Success edit status");
+            req.flash("alertStatus", "success");
+
+            res.redirect('/voucher');
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`);
+            req.flash('alertStatus', 'danger');
+            res.redirect('/voucher');            
+        }
+    }
 }
